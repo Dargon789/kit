ae703aa498cc84a1a5b8cd31afe3a629ae409d54
Samuele Agostinelli <batonenbois@gmail.com>
2024-12-16 12:33:02 -0500

    transak nft checkout (#199)
    * transak nft checkout
    
    * transak integration
    
    * regenerate lockfile
    
    * added missing network field to Transak
    
    * completed condition
    
    * transak proxy insertion into calldata

--- packages/checkout/src/utils/transak.ts
+++ packages/checkout/src/utils/transak.ts
@@ -1,6 +1,7 @@
 import { AddFundsSettings } from '../contexts'
 
 export const TRANSAK_API_KEY = '5911d9ec-46b5-48fa-a755-d59a715ff0cf'
+export const TRANSAK_PROXY_ADDRESS = '0x4a598b7ec77b1562ad0df7dc64a162695ce4c78a'
 
 export const getTransakLink = (addFundsSettings: AddFundsSettings) => {
   const defaultNetworks = 'ethereum,mainnet,arbitrum,optimism,polygon,polygonzkevm,zksync,base,bnb,oasys,astar,avaxcchain,immutablezkevm'
